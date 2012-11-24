8e54ded302aef24563644a440b204b40028cab6d
F.A. Bourbonnais <fbourbonnais@elapsetech.com>
2012-11-23 19:52:10 -0500

    Enlever le @RunWith inutile.

--- src/com/elapsetech/confsubmission/SubmissionServiceTest.java
+++ src/com/elapsetech/confsubmission/SubmissionServiceTest.java
@@ -1,9 +1,6 @@
 package com.elapsetech.confsubmission;
 
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
 public class SubmissionServiceTest {
 
 	
